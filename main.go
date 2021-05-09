0e3e0fb4a7e51cbc96f3b26516d8e0368a994c0a
jicowan <jicowan@hotmail.com>
2021-05-09 18:33:51 -0500

    export func

--- main.go
+++ main.go
@@ -1,12 +1,6 @@
 package fuzzy_pancake
 
-import "fmt"
-
-func main() {
-	fmt.Println(favs())
-}
-
-func favs() []string {
+func Favs() []string {
 	xs := []string{"Now is the time", "for all good men", "to come to the aid", "of their country"}
 	return xs
 }
